2395acd3648fd9d9b9e903ef118ba982bec14cde
unusuallyusual <66166465+unusuallyusual@users.noreply.github.com>
2022-02-02 08:45:27 +0300

    introduction to the profession .NET Developer

---
+++ les1/task2/HelloWorldConsoleApp2/HelloWorldConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Hello, ");
+      Console.Write("World ");
+      Console.Write("!!!");
+      Console.ReadKey();
+    }
+  }
+}
